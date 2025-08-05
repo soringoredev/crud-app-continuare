b43cc0b6436ba300901748abb700bf26870109b8
Sorin Gore <soringoredev@gmail.com>
2025-08-05 18:54:24 +0300

    Add getters and setters

--- src/main/java/pixel/academy/crud_app/entity/Student.java
+++ src/main/java/pixel/academy/crud_app/entity/Student.java
@@ -31,6 +31,37 @@ public class Student {
         this.email     = email;
     }
 
+    // definirea getters and setters
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }

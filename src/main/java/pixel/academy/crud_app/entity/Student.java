6fe3bc2e9dfe7ae1985edf384810d8c2763b895b
Sorin Gore <soringoredev@gmail.com>
2025-08-05 18:44:44 +0300

    Populate Student class

---
+++ src/main/java/pixel/academy/crud_app/entity/Student.java
@@ -0,0 +1,24 @@
+package pixel.academy.crud_app.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="student")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="email")
+    private String email;
+
+
+}
